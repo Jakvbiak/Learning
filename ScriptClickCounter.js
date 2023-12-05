3a7fa448894fb4283e7c9d335374af51664337db
Karolina Jakubiak <152705952+Jakvbiak@users.noreply.github.com>
2023-12-05 20:15:21 +0100

    Delete ScriptClickCounter.js

--- ScriptClickCounter.js
+++
@@ -1,8 +0,0 @@
-let clickButton = document.getElementById('clickButton')
-let clickCountDisplay = document.getElementById('clickCount')
-let clickCount = 0
-
-clickButton.addEventListener('click', function () {
-    clickCount += 1
-    clickCountDisplay.textContent = 'Click count: ' + clickCount
-})
